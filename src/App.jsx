0ba44efab11c83ffdcf6825ace26b447af21d7f3
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-02 18:16:50 +0100

    feat: remove unused hardcoded timezones

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,6 @@
 import { ActionDropdown } from '@/components/ActionDropdown.jsx'
 import { Timezones } from '@/components/Timezones.jsx'
 
-let timezones = [
-  {
-    timezone: 'Asia/Tokyo',
-    label: 'Tokyo Japan',
-    country: 'Japan',
-    countryCode: 'JP',
-    image: '',
-  },
-  {
-    timezone: 'America/New_York',
-    label: 'New York',
-    country: 'United States',
-    countryCode: 'US',
-    image: '',
-  },
-  {
-    timezone: 'Europe/London',
-    label: 'London',
-    country: 'England',
-    countryCode: 'GB',
-    image: '',
-  },
-  {
-    timezone: 'Europe/Belgrade',
-    label: 'Belgrade',
-    country: 'Serbia',
-    countryCode: 'RS',
-    image: '',
-  },
-]
-
 function App() {
   return (
     <div className="w-screen h-screen flex flex-nowrap overflow-hidden">
