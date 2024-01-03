abce88b6771afecdc947244329e20de3d4584471
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-03 23:07:40 +0100

    feat: add tabs and refactor color picking dialog

--- src/components/palette/PalettePicker.jsx
+++ src/components/palette/PalettePicker.jsx
@@ -1,7 +1,9 @@
+import { defaultPalettes } from '@/components/palette/colors.js'
 import { ScrollArea } from '@/components/ui/scroll-area.jsx'
+import { TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs.jsx'
 import { paletteActions } from '@/state/palette.js'
+import { Tabs } from '@radix-ui/react-tabs'
 import chroma from 'chroma-js'
-import { defaultPalettes } from './colors.js'
 import staticPalettes from './static-palettes.json'
 
 export function PalettePicker() {
@@ -23,39 +25,49 @@ export function PalettePicker() {
   }
 
   return (
-    <div className="w-[calc(100%_+3rem)] h-full max-h-[300px] -mx-6">
-      <ScrollArea className="h-[300px]">
-        <h3 className="pl-6 sticky top-0 bg-white">Dynamic (Daylight) palettes</h3>
+    <Tabs defaultValue="dynamic">
+      <TabsList>
+        <TabsTrigger value="dynamic">Daylight Palettes</TabsTrigger>
+        <TabsTrigger value="static">Static Palettes</TabsTrigger>
+      </TabsList>
 
-        <div className="flex flex-wrap gap-4 px-6 py-2">
-          {defaultPalettes.map(palette => (
-            <div
-              key={palette.id}
-              className="inline-flex w-40 h-10 rounded overflow-hidden cursor-pointer hover:ring-2 hover:ring-offset-1 hover:ring-primary shadow-sm"
-              onClick={() => setDynamicPalette(palette)}>
-              {palette.colors.map((color, index) => (
-                <div className="flex-1" key={palette.id + color + index} style={{ backgroundColor: color }}></div>
+      <TabsContent value="dynamic">
+        <div className="w-[calc(100%_+3rem)] h-full -mx-6">
+          <ScrollArea className="h-[280px]">
+            <div className="flex flex-wrap gap-4 px-6 py-2">
+              {defaultPalettes.map(palette => (
+                <div
+                  key={palette.id}
+                  className="inline-flex w-40 h-10 rounded overflow-hidden cursor-pointer hover:ring-2 hover:ring-offset-1 hover:ring-primary shadow-sm"
+                  onClick={() => setDynamicPalette(palette)}>
+                  {palette.colors.map((color, index) => (
+                    <div className="flex-1" key={palette.id + color + index} style={{ backgroundColor: color }}></div>
+                  ))}
+                </div>
               ))}
             </div>
-          ))}
+          </ScrollArea>
         </div>
+      </TabsContent>
 
-        <hr className="m-6" />
-
-        <h3 className="pl-6 sticky top-0 bg-white">Static palettes</h3>
-        <div className="flex flex-wrap gap-4 px-6 py-2">
-          {loadedPalettes.map(palette => (
-            <div
-              key={palette.id}
-              className="inline-flex rounded overflow-hidden cursor-pointer hover:ring-2 hover:ring-offset-1 hover:ring-primary shadow-sm"
-              onClick={() => setStaticPalette(palette)}>
-              {palette.colors.map(color => (
-                <div className="w-10 h-10" key={palette.id + color} style={{ backgroundColor: color }}></div>
+      <TabsContent value="static">
+        <div className="w-[calc(100%_+3rem)] h-full -mx-6">
+          <ScrollArea className="h-[280px]">
+            <div className="flex flex-wrap gap-4 px-6 py-2">
+              {loadedPalettes.map(palette => (
+                <div
+                  key={palette.id}
+                  className="inline-flex rounded overflow-hidden cursor-pointer hover:ring-2 hover:ring-offset-1 hover:ring-primary shadow-sm"
+                  onClick={() => setStaticPalette(palette)}>
+                  {palette.colors.map(color => (
+                    <div className="w-10 h-10" key={palette.id + color} style={{ backgroundColor: color }}></div>
+                  ))}
+                </div>
               ))}
             </div>
-          ))}
+          </ScrollArea>
         </div>
-      </ScrollArea>
-    </div>
+      </TabsContent>
+    </Tabs>
   )
 }
