704c0a5625f5be6f52d3182904bd4c6bb64f2192
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-09 00:23:44 +0100

    feat: add illustration for empty state

--- src/components/Timezones.jsx
+++ src/components/Timezones.jsx
@@ -3,8 +3,8 @@ import { Timezone } from '@/components/Timezone.jsx'
 import { Button } from '@/components/ui/button.jsx'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog.jsx'
 import { actions, useEpicZeitState } from '@/state/state.js'
-import { IconWorldSearch } from '@tabler/icons-react'
 import { When } from 'react-if'
+import earth from '../assets/earth.svg'
 
 export function Timezones() {
   const { currentTime, showResetTime, timezones } = useEpicZeitState()
@@ -12,15 +12,15 @@ export function Timezones() {
   return (
     <div className="w-full h-full flex">
       <When condition={timezones.length === 0}>
-        <div className="flex flex-auto flex-col justify-center items-center p-4">
-          <IconWorldSearch size={60} stroke={1} />
+        <div className="flex flex-auto flex-col justify-center items-center -mt-24">
+          <img src={earth} className="h-80" alt="empty state earth" />
 
-          <p className="mt-5 text-2xl">No location added yet</p>
-          <p className="mt-1 text-lg">Beautiful timezones are waiting for you</p>
+          <p className="mt-2 text-2xl">No location added yet</p>
+          <p className="mt-2 text-lg">Beautiful timezones are waiting for you</p>
 
           <Dialog>
             <DialogTrigger asChild>
-              <Button className="mt-5">Add first location</Button>
+              <Button className="mt-5 text-base font-normal">Add first location</Button>
             </DialogTrigger>
 
             <DialogContent className="overflow-hidden p-0 shadow-lg">
