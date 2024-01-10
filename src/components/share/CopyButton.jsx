bd2a1504d1c736f38fbef726eb57477137d5041f
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-10 21:47:27 +0100

    feat: refactor copy to clipboard button

--- src/components/share/CopyButton.jsx
+++ src/components/share/CopyButton.jsx
@@ -1,7 +1,5 @@
 import { Button } from '@/components/ui/button.jsx'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip.jsx'
 import { cn } from '@/utils.js'
-import { IconCheck, IconCopy, IconX } from '@tabler/icons-react'
 import copyToClipboard from 'clipboard-copy'
 import { useState } from 'react'
 import { Case, Switch } from 'react-if'
@@ -29,42 +27,20 @@ export function CopyButton({ value }) {
   }
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            size="sm"
-            className={cn(
-              'px-3',
-              copyState === COPY_STATE.SUCCESS ? 'bg-lime-500 hover:bg-lime-500/90' : '',
-              copyState === COPY_STATE.ERROR ? 'bg-red-500 hover:bg-red-500/90' : '',
-            )}
-            disabled={!value}
-            onClick={onCopyClick}>
-            <span className="sr-only">Copy</span>
-
-            <Switch>
-              <Case condition={copyState === COPY_STATE.INITIAL}>
-                <IconCopy size={20} stroke={1.5} />
-              </Case>
-              <Case condition={copyState === COPY_STATE.SUCCESS}>
-                <IconCheck size={20} stroke={1.5} />
-              </Case>
-              <Case condition={copyState === COPY_STATE.ERROR}>
-                <IconX size={20} stroke={1.5} />
-              </Case>
-            </Switch>
-          </Button>
-        </TooltipTrigger>
-
-        <TooltipContent onPointerDownOutside={event => event.preventDefault()}>
-          <Switch>
-            <Case condition={copyState === COPY_STATE.INITIAL}>Copy</Case>
-            <Case condition={copyState === COPY_STATE.SUCCESS}>Copied</Case>
-            <Case condition={copyState === COPY_STATE.ERROR}>Failed to copy</Case>
-          </Switch>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Button
+      size="sm"
+      className={cn(
+        'min-w-[90px] px-3',
+        copyState === COPY_STATE.SUCCESS ? 'bg-lime-500 hover:bg-lime-500/90' : '',
+        copyState === COPY_STATE.ERROR ? 'bg-red-500 hover:bg-red-500/90' : '',
+      )}
+      disabled={!value}
+      onClick={onCopyClick}>
+      <Switch>
+        <Case condition={copyState === COPY_STATE.INITIAL}>Copy link</Case>
+        <Case condition={copyState === COPY_STATE.SUCCESS}>Copied</Case>
+        <Case condition={copyState === COPY_STATE.ERROR}>Failed to copy</Case>
+      </Switch>
+    </Button>
   )
 }
