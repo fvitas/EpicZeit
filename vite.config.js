c33857df24f1167b87a127bfffc1251b2feab896
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-13 03:16:18 +0100

    feat: remove dependency

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,29 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), visualizer({ gzipSize: true })],
+  plugins: [react(), visualizer({ gzipSize: true })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
+  //   },
+  // },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     sourcemap: false,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about', 'index.html'),
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'dayjs'],
