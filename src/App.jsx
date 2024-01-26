85ff3c4c8614d3dc9abfe45d65175908f6825c22
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-26 23:03:30 +0100

    feat: move app related code into app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './globals.css'
+import '/node_modules/flag-icons/css/flag-icons.min.css'
+
 import { ActionDropdown } from '@/components/ActionDropdown.jsx'
 import { Timezones } from '@/components/Timezones.jsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+  },
+})
 
 function App() {
   return (
     <div className="w-screen h-screen flex flex-nowrap overflow-scroll">
-      <ActionDropdown />
-      <Timezones />
+      <QueryClientProvider client={queryClient}>
+        <ActionDropdown />
+        <Timezones />
+      </QueryClientProvider>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './globals.css'
-import '/node_modules/flag-icons/css/flag-icons.min.css'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-    },
-  },
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 )
