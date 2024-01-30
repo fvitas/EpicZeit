dafbb9eafd54ec7168ca1156419291cce3c14bb4
Filip Vitas <filip.vitas.dev@gmail.com>
2024-01-30 23:47:59 +0100

    feat: init turborepo and remove useless files

--- packages/ui/src/card.jsx
+++ packages/ui/src/card.jsx
@@ -3,12 +3,7 @@ export function Card({
   title,
   children,
   href,
-}: {
-  className?: string;
-  title: string;
-  children: React.ReactNode;
-  href: string;
-}): JSX.Element {
+}) {
   return (
     <a
       className={className}
